9e898757bbe056ad66bf6525b72baa5f5de68632
James Titcumb <james@asgrim.com>
2019-01-31 09:58:31 +0000

    Running the process but not changing icon

--- indicator.py
+++ indicator.py
@@ -5,22 +5,53 @@ from gi.repository import Gtk
 from gi.repository import AppIndicator3
 import signal
 import os
+import subprocess
 
 INDICATOR_NAME = 'script-running-indicator'
 INDICATOR_ICON_STOPPED = os.path.abspath('stopped.svg')
+INDICATOR_ICON_RUNNING = os.path.abspath('running.svg')
+INDICATOR_ICON_STARTING = os.path.abspath('starting.svg')
+SCRIPT_TO_RUN = ['sleep', '1']
+
+item_start = None
+indicator = None  # type: AppIndicator3.Indicator
+running_process = None
+
 
 def build_menu():
+    global item_start
     menu = Gtk.Menu()
-    item_quit = Gtk.MenuItem('Close this tool')
+    item_start = Gtk.MenuItem('Run the script')
+    item_start.connect('activate', start_process)
+    item_quit = Gtk.MenuItem('Quit')
     item_quit.connect('activate', quit)
+    menu.append(item_start)
     menu.append(item_quit)
     menu.show_all()
     return menu
 
+
 def quit(source):
     Gtk.main_quit()
 
+
+def start_process(source):
+    global running_process
+    print('starting')
+    indicator.set_icon(INDICATOR_ICON_STARTING)
+
+    print('actually starting')
+    running_process = subprocess.Popen(SCRIPT_TO_RUN)
+    while running_process.poll() is None:
+        print('running')
+        indicator.set_icon(INDICATOR_ICON_RUNNING)
+
+    print('stopped')
+    indicator.set_icon(INDICATOR_ICON_STOPPED)
+
+
 def main():
+    global indicator
     indicator = AppIndicator3.Indicator.new(INDICATOR_NAME, INDICATOR_ICON_STOPPED,
                                             AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)

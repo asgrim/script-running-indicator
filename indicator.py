c266f7c8bb3a1e4df5d92300bbf2598d2f9e9389
James Titcumb <james@asgrim.com>
2019-01-31 10:40:20 +0000

    Move into an object because whatever

--- indicator.py
+++ indicator.py
@@ -12,58 +12,46 @@ INDICATOR_ICON_RUNNING = os.path.abspath('running.svg')
 INDICATOR_ICON_STARTING = os.path.abspath('starting.svg')
 SCRIPT_TO_RUN = ['sleep', '15']
 
-item_start = None
-indicator = None  # type: AppIndicator3.Indicator
-running_indicator_set = False
-running_process = None
+
+class Indicator():
+    def __init__(self):
+        self.indicator = AppIndicator3.Indicator.new(INDICATOR_NAME, INDICATOR_ICON_STOPPED,
+                                                AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
+        self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+        self.indicator.set_menu(self.build_menu())
+        self.running_process = None
+
+    def build_menu(self):
+        menu = Gtk.Menu()
+        item_start = Gtk.MenuItem('Run the script')
+        item_start.connect('activate', self.start_process)
+        item_quit = Gtk.MenuItem('Quit')
+        item_quit.connect('activate', self.quit)
+        menu.append(item_start)
+        menu.append(item_quit)
+        menu.show_all()
+        return menu
+
+    def start_process(self, source):
+        running_indicator_set = False
+        self.indicator.set_icon_full(INDICATOR_ICON_STARTING, 'Starting')
+        self.indicator.set_label('HELLO')
+        print('starting')
+
+        self.running_process = subprocess.Popen(SCRIPT_TO_RUN)
+        while self.running_process.poll() is None:
+            if not running_indicator_set:
+                print('running')
+                running_indicator_set = True
+                self.indicator.set_icon_full(INDICATOR_ICON_RUNNING, 'Running')
+
+        print('stopped')
+        self.indicator.set_icon_full(INDICATOR_ICON_STOPPED, 'Stopped')
+
+    def quit(self, source):
+        Gtk.main_quit()
 
 
-def build_menu():
-    global item_start
-    menu = Gtk.Menu()
-    item_start = Gtk.MenuItem('Run the script')
-    item_start.connect('activate', start_process)
-    item_quit = Gtk.MenuItem('Quit')
-    item_quit.connect('activate', quit)
-    menu.append(item_start)
-    menu.append(item_quit)
-    menu.show_all()
-    return menu
-
-
-def quit(source):
-    Gtk.main_quit()
-
-
-def start_process(source):
-    global running_process, running_indicator_set
-    running_indicator_set = False
-    indicator.set_icon(INDICATOR_ICON_STARTING)
-    print('starting')
-
-    running_process = subprocess.Popen(SCRIPT_TO_RUN)
-    while running_process.poll() is None:
-        if not running_indicator_set:
-            print('running')
-            running_indicator_set = True
-            indicator.set_icon(INDICATOR_ICON_RUNNING)
-
-    print('stopped')
-    indicator.set_icon(INDICATOR_ICON_STOPPED)
-
-
-def main():
-    global indicator
-
-    indicator = AppIndicator3.Indicator.new(INDICATOR_NAME, INDICATOR_ICON_STOPPED,
-                                            AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
-    indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-    indicator.set_menu(build_menu())
-
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-
-    Gtk.main()
-
-
-if __name__ == "__main__":
-    main()
+Indicator()
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+Gtk.main()

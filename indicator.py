8e5610d5828125296af8b1c53738291724c9f3c6
James Titcumb <james@asgrim.com>
2019-01-31 09:26:55 +0000

    Added ability to quit from a menu

--- indicator.py
+++ indicator.py
@@ -8,12 +8,22 @@ import signal
 INDICATOR_NAME = 'script-running-indicator'
 INDICATOR_ICON = 'none set'
 
+def build_menu():
+    menu = Gtk.Menu()
+    item_quit = Gtk.MenuItem('Close this tool')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+def quit(source):
+    Gtk.main_quit()
 
 def main():
     indicator = AppIndicator3.Indicator.new(INDICATOR_NAME, INDICATOR_ICON,
                                             AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-    indicator.set_menu(Gtk.Menu())
+    indicator.set_menu(build_menu())
 
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 

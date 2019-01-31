68464c987e58e0cf576a7a772a5bd5d1db782246
James Titcumb <james@asgrim.com>
2019-01-31 10:16:59 +0000

    Only set icon once

--- indicator.py
+++ indicator.py
@@ -1,8 +1,7 @@
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
-from gi.repository import Gtk
-from gi.repository import AppIndicator3
+from gi.repository import Gtk, AppIndicator3
 import signal
 import os
 import subprocess
@@ -11,10 +10,11 @@ INDICATOR_NAME = 'script-running-indicator'
 INDICATOR_ICON_STOPPED = os.path.abspath('stopped.svg')
 INDICATOR_ICON_RUNNING = os.path.abspath('running.svg')
 INDICATOR_ICON_STARTING = os.path.abspath('starting.svg')
-SCRIPT_TO_RUN = ['sleep', '1']
+SCRIPT_TO_RUN = ['sleep', '15']
 
 item_start = None
 indicator = None  # type: AppIndicator3.Indicator
+running_indicator_set = False
 running_process = None
 
 
@@ -36,15 +36,17 @@ def quit(source):
 
 
 def start_process(source):
-    global running_process
-    print('starting')
+    global running_process, running_indicator_set
+    running_indicator_set = False
     indicator.set_icon(INDICATOR_ICON_STARTING)
+    print('starting')
 
-    print('actually starting')
     running_process = subprocess.Popen(SCRIPT_TO_RUN)
     while running_process.poll() is None:
-        print('running')
-        indicator.set_icon(INDICATOR_ICON_RUNNING)
+        if not running_indicator_set:
+            print('running')
+            running_indicator_set = True
+            indicator.set_icon(INDICATOR_ICON_RUNNING)
 
     print('stopped')
     indicator.set_icon(INDICATOR_ICON_STOPPED)
@@ -52,6 +54,7 @@ def start_process(source):
 
 def main():
     global indicator
+
     indicator = AppIndicator3.Indicator.new(INDICATOR_NAME, INDICATOR_ICON_STOPPED,
                                             AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
